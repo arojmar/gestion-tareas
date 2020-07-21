76e22cce54a262653d617e172112644892dfb3ab
arojmar <arojmar@gmail.com>
2020-07-21 14:10:53 +0200

    Añadidas dependencias en fichero app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainViewComponent } from './main-view/main-view.component';
-<<<<<<< HEAD
 import { ListComponent } from './main-view/list/list.component';
-=======
 import { CheckboxComponent } from './main-view/checkbox/checkbox.component';
->>>>>>> main-view/checkbox
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MainViewComponent,
-<<<<<<< HEAD
-    ListComponent
-=======
+    ListComponent,
     CheckboxComponent
->>>>>>> main-view/checkbox
   ],
   imports: [
     BrowserModule,
